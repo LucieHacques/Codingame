e4269ff85022760d8a9d527eb9d78658a87a7799
Lucie Hacques <lucie.hacques@gmail.com>
2017-06-19 16:47:24 +0200

    class Game updated

--- src/core/main/Game.java
+++ src/core/main/Game.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
+	
+	static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+	static ArrayList<String> asciiTab = new ArrayList<>();
 
 	public static void main(String[] args) {
 
@@ -14,25 +18,54 @@ public class Game {
         System.out.println("Veuillez saisir la hauteur des lettres : ");
         int H = scanner.nextInt();
         
-        System.out.println("Veuillez saisir une lettre de l'alphabet");
-        String caractere = scanner.next();
-        
+        System.out.println("Veuillez saisir une lettre de l'alphabet : ");
+        String caractere = scanner.next().toUpperCase();
+
         while (caractere.length()>1)
         {
-        	System.out.println("Veuillez saisir seulement une lettre de l'alphabet");
+        	System.out.println("Veuillez saisir seulement une lettre de l'alphabet : ");
             caractere = scanner.next();
         }
         
         System.out.println("Veuillez saisir une chaine de caractère : ");
-        String chaine = scanner.next();
+        String chaine = scanner.next().toUpperCase();
         
-        showResultCharacter(caractere);
+        for(int i = 0; i < H; i++) {
+			asciiTab.add(scanner.nextLine());
+		}
         
+        buildCharacter(caractere, L, H);
+        buildText(chaine, L, H);
+        
+        scanner.close();
 	}
 	
-	public static void showResultCharacter(String caractere)
+	public static void buildCharacter(String caractere, int width, int height)
 	{
 		
+	}
+	
+	public static void buildText(String text, int width, int height)
+	{	
+		ArrayList<Integer> listIndex = new ArrayList<Integer>();
+		for(char c : text.toCharArray()) {
+			if(c < 'A' || c > 'Z') listIndex.add(26);
+			else {
+				for(char letter : alphabet.toCharArray()) {
+					if(c == letter) {
+						listIndex.add(alphabet.indexOf(letter));
+					}
+				}
+			}
+		}
+		
+		for(int i=0; i<height; i++){
+			for(int j = 0; j < listIndex.size(); j++) {
+				System.out.print(asciiTab.get(i).substring(listIndex.get(j)*width, listIndex.get(j)*width + width));
+			}
+		}
+		
+		System.out.println();
 	}
 
 }
--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,6 +1,13 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.junit.Test;
+
+import com.github.javafaker.Faker;
+
+import main.Game;
 
 public class GameTest {
 
@@ -9,4 +16,52 @@ public class GameTest {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void testbuildCharacter() {
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+		
+		Boolean isOk = false;
+		
+		Game game = new Game();
+		
+		Faker faker = new Faker();
+		
+        int L = 4;
+        int H = 5;
+
+        int index = faker.number().numberBetween(0, 10);
+        String caractere = faker.lorem().characters(index).toUpperCase();
+        if (caractere.toUpperCase().contains(alphabet)){
+        	isOk=true;
+        }
+		
+		assertTrue(isOk);
+	}
+	
+	@Test
+	public void testbuildText() {
+		
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+		
+		Boolean isOk = true;
+		
+		Game game = new Game();
+		
+		Faker faker = new Faker();
+		
+        int L = 4;
+        int H = 5;
+
+        String chaine = faker.lorem().toString().toUpperCase();
+        int index = faker.number().numberBetween(0, 10);
+        String caractere = faker.lorem().characters(index).toUpperCase();
+        for (char c:chaine.toCharArray()){
+        	if (!caractere.toUpperCase().contains(alphabet)){
+            	isOk=false;
+            }
+        }
+        
+		
+		assertTrue(isOk);
+	}
 }
